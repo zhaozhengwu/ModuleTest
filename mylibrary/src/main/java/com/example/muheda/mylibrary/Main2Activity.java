6e99b86d53e879592b184680a4cf12b2fa99170a
zhaozhengwu <zhw.zhao@muheda.com>
2018-06-27 17:35:55 +0800

    dd

--- mylibrary/src/main/java/com/example/muheda/mylibrary/Main2Activity.java
+++ mylibrary/src/main/java/com/example/muheda/mylibrary/Main2Activity.java
@@ -1,5 +1,6 @@
 package com.example.muheda.mylibrary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent intent=new Intent("com.example.muheda.mynewlibrary.MainNewActivity");
+        startActivity(intent);
+
     }
 }
