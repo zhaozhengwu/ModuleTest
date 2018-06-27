766027f9660d218dff2d3c94982c3fc56d64864a
zhaozhengwu <zhw.zhao@muheda.com>
2018-06-27 14:33:21 +0800

    ssd

---
+++ app/src/main/java/com/example/muheda/moduletest/Utils.java
@@ -0,0 +1,11 @@
+package com.example.muheda.moduletest;
+
+/**
+ * Created by 13660 on 2018/6/27.
+ */
+
+public class Utils {
+    public static void put(){
+
+    }
+}
---
+++ mylibrary/src/main/java/com/example/muheda/mylibrary/Util.java
@@ -0,0 +1,11 @@
+package com.example.muheda.mylibrary;
+
+/**
+ * Created by 13660 on 2018/6/27.
+ */
+
+public class Util {
+    public static void put(){
+
+    }
+}
